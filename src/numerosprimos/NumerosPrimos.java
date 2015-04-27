6ca033743414065a029a0668f56b74e690ce3c40
media <media@Pc-Fran>
2015-04-27 08:58:15 +0200

    Clases creadas

---
+++ src/numerosprimos/NumerosPrimos.java
@@ -0,0 +1,16 @@
+package numerosprimos;
+
+/**
+ *
+ * @author @FranJAbrilA <ffrannabril@gmail.com>
+ */
+public class NumerosPrimos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
